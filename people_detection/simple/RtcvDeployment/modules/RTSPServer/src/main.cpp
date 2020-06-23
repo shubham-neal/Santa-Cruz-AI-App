662fbaa1fd7fc881ee4b2185add9e52aee645703
Ubuntu <boris@brainbox.xovp4re31zleddnmnnhg10byqb.xx.internal.cloudapp.net>
2020-06-23 06:23:49 +0000

    rtsp server example

--- people_detection/simple/RtcvDeployment/modules/RTSPServer/src/main.cpp
+++ people_detection/simple/RtcvDeployment/modules/RTSPServer/src/main.cpp
@@ -21,27 +21,91 @@
 
 #include <gst/rtsp-server/rtsp-server.h>
 
-/* define this if you want the resource to only be available when using
- * user/password as the password */
-#undef WITH_AUTH
+#define DEFAULT_RTSP_PORT "8554"
 
-/* define this if you want the server to use TLS (it will also need WITH_AUTH
- * to be defined) */
-#undef WITH_TLS
+static char *port = (char *) DEFAULT_RTSP_PORT;
 
-/* this timeout is periodically run to clean up the expired sessions from the
- * pool. This needs to be run explicitly currently but might be done
- * automatically as part of the mainloop. */
-static gboolean
-timeout (GstRTSPServer * server)
+static GOptionEntry entries[] = {
+  {"port", 'p', 0, G_OPTION_ARG_STRING, &port,
+      "Port to listen on (default: " DEFAULT_RTSP_PORT ")", "PORT"},
+  {NULL}
+};
+
+/* called when a stream has received an RTCP packet from the client */
+static void
+on_ssrc_active (GObject * session, GObject * source, GstRTSPMedia * media)
 {
-  GstRTSPSessionPool *pool;
+  GstStructure *stats;
 
-  pool = gst_rtsp_server_get_session_pool (server);
-  gst_rtsp_session_pool_cleanup (pool);
-  g_object_unref (pool);
+  GST_INFO ("source %p in session %p is active", source, session);
 
-  return TRUE;
+  g_object_get (source, "stats", &stats, NULL);
+  if (stats) {
+    gchar *sstr;
+
+    sstr = gst_structure_to_string (stats);
+    g_print ("structure: %s\n", sstr);
+    g_free (sstr);
+
+    gst_structure_free (stats);
+  }
+}
+
+static void
+on_sender_ssrc_active (GObject * session, GObject * source,
+    GstRTSPMedia * media)
+{
+  GstStructure *stats;
+
+  GST_INFO ("source %p in session %p is active", source, session);
+
+  g_object_get (source, "stats", &stats, NULL);
+  if (stats) {
+    gchar *sstr;
+
+    sstr = gst_structure_to_string (stats);
+    g_print ("Sender stats:\nstructure: %s\n", sstr);
+    g_free (sstr);
+
+    gst_structure_free (stats);
+  }
+}
+
+/* signal callback when the media is prepared for streaming. We can get the
+ * session manager for each of the streams and connect to some signals. */
+static void
+media_prepared_cb (GstRTSPMedia * media)
+{
+  guint i, n_streams;
+
+  n_streams = gst_rtsp_media_n_streams (media);
+
+  GST_INFO ("media %p is prepared and has %u streams", media, n_streams);
+
+  for (i = 0; i < n_streams; i++) {
+    GstRTSPStream *stream;
+    GObject *session;
+
+    stream = gst_rtsp_media_get_stream (media, i);
+    if (stream == NULL)
+      continue;
+
+    session = gst_rtsp_stream_get_rtpsession (stream);
+    GST_INFO ("watching session %p on stream %u", session, i);
+
+    g_signal_connect (session, "on-ssrc-active",
+        (GCallback) on_ssrc_active, media);
+    g_signal_connect (session, "on-sender-ssrc-active",
+        (GCallback) on_sender_ssrc_active, media);
+  }
+}
+
+static void
+media_configure_cb (GstRTSPMediaFactory * factory, GstRTSPMedia * media)
+{
+  /* connect our prepared signal so that we can see when this media is
+   * prepared for streaming */
+  g_signal_connect (media, "prepared", (GCallback) media_prepared_cb, factory);
 }
 
 int
@@ -51,101 +115,50 @@ main (int argc, char *argv[])
   GstRTSPServer *server;
   GstRTSPMountPoints *mounts;
   GstRTSPMediaFactory *factory;
-#ifdef WITH_AUTH
-  GstRTSPAuth *auth;
-  GstRTSPToken *token;
-  gchar *basic;
-  GstRTSPPermissions *permissions;
-#endif
-#ifdef WITH_TLS
-  GTlsCertificate *cert;
+  GOptionContext *optctx;
   GError *error = NULL;
-#endif
+  gchar *str;
 
-  gst_init (&argc, &argv);
+  optctx = g_option_context_new ("<filename.mp4> - Test RTSP Server, MP4");
+  g_option_context_add_main_entries (optctx, entries, NULL);
+  g_option_context_add_group (optctx, gst_init_get_option_group ());
+  if (!g_option_context_parse (optctx, &argc, &argv, &error)) {
+    g_printerr ("Error parsing options: %s\n", error->message);
+    g_option_context_free (optctx);
+    g_clear_error (&error);
+    return -1;
+  }
+
+  if (argc < 2) {
+    g_print ("%s\n", g_option_context_get_help (optctx, TRUE, NULL));
+    return 1;
+  }
+  g_option_context_free (optctx);
 
   loop = g_main_loop_new (NULL, FALSE);
 
   /* create a server instance */
   server = gst_rtsp_server_new ();
-
-#ifdef WITH_AUTH
-  /* make a new authentication manager. it can be added to control access to all
-   * the factories on the server or on individual factories. */
-  auth = gst_rtsp_auth_new ();
-#ifdef WITH_TLS
-  cert = g_tls_certificate_new_from_pem ("-----BEGIN CERTIFICATE-----"
-      "MIICJjCCAY+gAwIBAgIBBzANBgkqhkiG9w0BAQUFADCBhjETMBEGCgmSJomT8ixk"
-      "ARkWA0NPTTEXMBUGCgmSJomT8ixkARkWB0VYQU1QTEUxHjAcBgNVBAsTFUNlcnRp"
-      "ZmljYXRlIEF1dGhvcml0eTEXMBUGA1UEAxMOY2EuZXhhbXBsZS5jb20xHTAbBgkq"
-      "hkiG9w0BCQEWDmNhQGV4YW1wbGUuY29tMB4XDTExMDExNzE5NDcxN1oXDTIxMDEx"
-      "NDE5NDcxN1owSzETMBEGCgmSJomT8ixkARkWA0NPTTEXMBUGCgmSJomT8ixkARkW"
-      "B0VYQU1QTEUxGzAZBgNVBAMTEnNlcnZlci5leGFtcGxlLmNvbTBcMA0GCSqGSIb3"
-      "DQEBAQUAA0sAMEgCQQDYScTxk55XBmbDM9zzwO+grVySE4rudWuzH2PpObIonqbf"
-      "hRoAalKVluG9jvbHI81eXxCdSObv1KBP1sbN5RzpAgMBAAGjIjAgMAkGA1UdEwQC"
-      "MAAwEwYDVR0lBAwwCgYIKwYBBQUHAwEwDQYJKoZIhvcNAQEFBQADgYEAYx6fMqT1"
-      "Gvo0jq88E8mc+bmp4LfXD4wJ7KxYeadQxt75HFRpj4FhFO3DOpVRFgzHlOEo3Fwk"
-      "PZOKjvkT0cbcoEq5whLH25dHoQxGoVQgFyAP5s+7Vp5AlHh8Y/vAoXeEVyy/RCIH"
-      "QkhUlAflfDMcrrYjsmwoOPSjhx6Mm/AopX4="
-      "-----END CERTIFICATE-----"
-      "-----BEGIN PRIVATE KEY-----"
-      "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEA2EnE8ZOeVwZmwzPc"
-      "88DvoK1ckhOK7nVrsx9j6TmyKJ6m34UaAGpSlZbhvY72xyPNXl8QnUjm79SgT9bG"
-      "zeUc6QIDAQABAkBRFJZ32VbqWMP9OVwDJLiwC01AlYLnka0mIQZbT/2xq9dUc9GW"
-      "U3kiVw4lL8v/+sPjtTPCYYdzHHOyDen6znVhAiEA9qJT7BtQvRxCvGrAhr9MS022"
-      "tTdPbW829BoUtIeH64cCIQDggG5i48v7HPacPBIH1RaSVhXl8qHCpQD3qrIw3FMw"
-      "DwIga8PqH5Sf5sHedy2+CiK0V4MRfoU4c3zQ6kArI+bEgSkCIQCLA1vXBiE31B5s"
-      "bdHoYa1BXebfZVd+1Hd95IfEM5mbRwIgSkDuQwV55BBlvWph3U8wVIMIb4GStaH8"
-      "W535W8UBbEg=" "-----END PRIVATE KEY-----", -1, &error);
-  if (cert == NULL) {
-    g_printerr ("failed to parse PEM: %s\n", error->message);
-    return -1;
-  }
-  gst_rtsp_auth_set_tls_certificate (auth, cert);
-  g_object_unref (cert);
-#endif
-
-  /* make user token */
-  token =
-      gst_rtsp_token_new (GST_RTSP_TOKEN_MEDIA_FACTORY_ROLE, G_TYPE_STRING,
-      "user", NULL);
-  basic = gst_rtsp_auth_make_basic ("user", "password");
-  gst_rtsp_auth_add_basic (auth, basic, token);
-  g_free (basic);
-  gst_rtsp_token_unref (token);
-
-  /* configure in the server */
-  gst_rtsp_server_set_auth (server, auth);
-#endif
+  g_object_set (server, "service", port, NULL);
 
   /* get the mount points for this server, every server has a default object
    * that be used to map uri mount points to media factories */
   mounts = gst_rtsp_server_get_mount_points (server);
 
+  str = g_strdup_printf ("( "
+      "filesrc location=\"%s\" ! qtdemux name=d "
+      "d. ! queue ! rtph264pay pt=96 name=pay0 "
+      "d. ! queue ! rtpmp4apay pt=97 name=pay1 " ")", argv[1]);
+
   /* make a media factory for a test stream. The default media factory can use
-   * gst-launch syntax to create pipelines.
+   * gst-launch syntax to create pipelines. 
    * any launch line works as long as it contains elements named pay%d. Each
    * element with pay%d names will be a stream */
   factory = gst_rtsp_media_factory_new ();
-  gst_rtsp_media_factory_set_launch (factory, "( "
-      "videotestsrc ! video/x-raw,width=352,height=288,framerate=15/1 ! "
-      "x264enc ! rtph264pay name=pay0 pt=96 "
-/*      "audiotestsrc ! audio/x-raw,rate=8000 ! "
-      "alawenc ! rtppcmapay name=pay1 pt=97 " 
-*/      
-      ")");
-#ifdef WITH_AUTH
-  /* add permissions for the user media role */
-  permissions = gst_rtsp_permissions_new ();
-  gst_rtsp_permissions_add_role (permissions, "user",
-      GST_RTSP_PERM_MEDIA_FACTORY_ACCESS, G_TYPE_BOOLEAN, TRUE,
-      GST_RTSP_PERM_MEDIA_FACTORY_CONSTRUCT, G_TYPE_BOOLEAN, TRUE, NULL);
-  gst_rtsp_media_factory_set_permissions (factory, permissions);
-  gst_rtsp_permissions_unref (permissions);
-#ifdef WITH_TLS
-  gst_rtsp_media_factory_set_profiles (factory, GST_RTSP_PROFILE_SAVP);
-#endif
-#endif
+  gst_rtsp_media_factory_set_launch (factory, str);
+  g_signal_connect (factory, "media-configure", (GCallback) media_configure_cb,
+      factory);
+  g_free (str);
 
   /* attach the test factory to the /test url */
   gst_rtsp_mount_points_add_factory (mounts, "/test", factory);
@@ -154,26 +167,11 @@ main (int argc, char *argv[])
   g_object_unref (mounts);
 
   /* attach the server to the default maincontext */
-  if (gst_rtsp_server_attach (server, NULL) == 0)
-    goto failed;
+  gst_rtsp_server_attach (server, NULL);
 
-  /* add a timeout for the session cleanup */
-  g_timeout_add_seconds (2, (GSourceFunc) timeout, server);
-
-  /* start serving, this never stops */
-#ifdef WITH_TLS
-  g_print ("stream ready at rtsps://127.0.0.1:8554/test\n");
-#else
-  g_print ("stream ready at rtsp://127.0.0.1:8554/test\n");
-#endif
+  /* start serving */
+  g_print ("stream ready at rtsp://127.0.0.1:%s/test\n", port);
   g_main_loop_run (loop);
 
   return 0;
-
-  /* ERRORS */
-failed:
-  {
-    g_print ("failed to attach the server\n");
-    return -1;
-  }
 }
