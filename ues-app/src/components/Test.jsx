4fe2bf4bcb5e713449d8274cbc5bc7fdef4e0750
Anton Bursch <antonphd@gmail.com>
2020-06-27 13:22:22 -0700

    adding Test component

---
+++ ues-app/src/components/Test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export class Test extends React.Component {
+    static defaultProps = {
+        
+    }
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentDidUpdate() {
+        
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+            </React.Fragment>
+        );
+    }
+}
