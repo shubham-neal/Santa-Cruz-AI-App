6de31596ef04ea47d692814f04163679284613b3
Anton Bursch <antonphd@gmail.com>
2020-07-10 17:24:11 -0700

    clear zone polygon

--- ues-app/src/components/EditZones.jsx
+++ ues-app/src/components/EditZones.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Label } from 'office-ui-fabric-react/lib/Label';
 import { TextField } from 'office-ui-fabric-react/lib/TextField';
+import { DefaultButton } from '@fluentui/react/lib/Button';
 
 export class EditZones extends React.Component {
     static defaultProps = {
@@ -38,12 +39,29 @@ export class EditZones extends React.Component {
                         <Label style={{ fontWeight: 'bold' }}>Zones</Label>
                     </div>
                     <form>
-                        <TextField
-                            defaultValue={this.state.aggregator.zones[this.state.selectedZoneIndex].name}
-                            onChange={(e) => {
-                                this.state.aggregator.zones[this.state.selectedZoneIndex].name = e.target.value;
-                            }}
-                        />
+                        <table>
+                            <tr>
+                                <td>
+                                    <TextField
+                                        defaultValue={this.state.aggregator.zones[this.state.selectedZoneIndex].name}
+                                        onChange={(e) => {
+                                            this.state.aggregator.zones[this.state.selectedZoneIndex].name = e.target.value;
+                                            this.props.updateAggregator(this.state.aggregator);
+                                        }}
+                                    />
+                                </td>
+                                <td>
+                                    <DefaultButton
+                                        onClick={(e) => {
+                                            this.state.aggregator.zones[this.state.selectedZoneIndex].polygon = [];
+                                            this.props.updateAggregator(this.state.aggregator);
+                                        }}
+                                    >
+                                        Clear
+                                    </DefaultButton>
+                                </td>
+                            </tr>
+                        </table>
                     </form>
                 </div>
             </React.Fragment>
