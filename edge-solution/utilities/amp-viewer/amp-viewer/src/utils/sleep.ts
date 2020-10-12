012ab88aec475d67c38234c32f1e947120599bb8
Edwin Webster <edwin@nealanalytics.com>
2020-10-12 15:54:48 -0700

    Removing LVA references

--- edge-solution/utilities/amp-viewer/amp-viewer/src/utils/sleep.ts
+++
@@ -1,7 +0,0 @@
-export function sleep(milliseconds: number): Promise<void> {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            return resolve();
-        }, milliseconds);
-    });
-}
