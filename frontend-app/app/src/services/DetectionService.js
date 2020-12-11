57ddc1bc312576bf9bcbd4850357b30717054d09
AntonClaytonBursch <v-abursch@microsoft.com>
2020-12-10 17:18:37 -0800

    developing detection service

--- frontend-app/app/src/services/DetectionService.js
+++ frontend-app/app/src/services/DetectionService.js
@@ -7,60 +7,27 @@ export class DetectionService {
         this.containerName = containerName;
     }
 
-    getDetections(time, minutesBefore, minutesAfter) {
-        const detections = [];
-        const dates = this.calculateDateRange(time, minutesBefore, minutesAfter);
-        for(const date in dates) {
-            const inferences = this.getInferences(date);
-            for(const inference in inferences) {
-                detections.push(inference);
-            }
-        }
+    async blobExists(blobName) {
+        const containerClient = this.blobServiceClient.getContainerClient(this.containerName);
+        const blobClient = containerClient.getBlobClient(blobName);
+        const exists = blobClient.exists();
+        return exists;
     }
 
-    // TODO: account for daylight saving
-    // TODO: optimize finding partition
-    getInferences(date) {
-        const inferences = [];
-
-        let hours = date.getUTCHours();
-        if(hours.length === 1) {
-            hours = `0${hours}`;
-        }
-        let minutes = date.getUTCMinutes();
-        if(minutes.length === 1) {
-            minutes = `0${minutes}`;
-        }
-        let blobName = `${this.iotHubName}/0${this.findPartition()}/${date.toLocaleDateString('fr-CA', {
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit'
-        }).replace(/-/g, '/')}/${hours}/${minutes}`;
-
-        const exists = await this.blobExists(this.containerName, blobName);
-        if(exists) {
-            const containerClient = this.blobServiceClient.getContainerClient(this.containerName);
-            let iter = containerClient.listBlobsByHierarchy("/", { prefix: blobName });
-            for await (const item of iter) {
-                const blobs = await this.downloadBlob(this.containerName, item.name);
-                for (const blob in blobs) {
-                    const inferences = blob.inferences;
-                    for (const inference in inferences) {
-                        inference.in = blob.in === 0 ? true : false;
-                        inference.out = blob.out === 0 ? true: false;
-                        inferences.push(inference);
-                    }
-                }
-            }
-        }
-
-        return inferences;
+    async blobToString(blob) {
+        const fileReader = new FileReader();
+        return new Promise((resolve, reject) => {
+            fileReader.onloadend = (ev) => {
+                resolve(ev.target.result);
+            };
+            fileReader.onerror = reject;
+            fileReader.readAsText(blob);
+        });
     }
-
 
     calculateDateRange(time, minutesBefore, minutesAfter) {
         const dates = [];
-        for(let i = -minutesBefore; i <= minutesAfter; i++) {
+        for (let i = -minutesBefore; i <= minutesAfter; i++) {
             const date = new Date(time);
             date.setMinutes(date.getMinutes() + i);
             dates.push(date);
@@ -77,61 +44,103 @@ export class DetectionService {
         }
     }
 
-    async blobExists(blobName) {
-        const containerClient = this.blobServiceClient.getContainerClient(this.containerName);
-        const blobClient = containerClient.getBlobClient(blobName);
-        const exists = blobClient.exists();
-        return exists;
+    getDetections(time, minutesBefore, minutesAfter) {
+        const detections = [];
+        const dates = this.calculateDateRange(time, minutesBefore, minutesAfter);
+        for (const date in dates) {
+            const inferences = this.getInferences(date);
+            for (const inference in inferences) {
+                detections.push(inference);
+            }
+        }
     }
-    
-    async downloadBlob(containerName, blobName) {
-        const containerClient = this.blobServiceClient.getContainerClient(containerName);
-        const blobClient = containerClient.getBlobClient(blobName);
-        const downloadBlockBlobResponse = await blobClient.download();
 
-        const downloaded = await this.blobToString(await downloadBlockBlobResponse.blobBody);
-        const views = downloaded.replace(/\\"/g, /'/).split('\r\n');
+    // TODO: account for daylight saving
+    // TODO: optimize finding partition
+    getInferences(date) {
+        const inferences = [];
 
-        const frames = [];
-        const l = views.length;
-        for (let i = 0; i < l; i++) {
-            const view = views[i];
-            if (view && view !== undefined && view !== "") {
-                let parsedView = JSON.parse(view);
-                if (parsedView.hasOwnProperty('Body')) {
-                    let body = parsedView.Body;
-                    if (body && body !== undefined && body !== "") {
-                        let decodedBody = atob(body);
-                        if (decodedBody && decodedBody !== undefined && decodedBody !== "") {
-                            try {
-                                let parsedBody = JSON.parse(decodedBody.replace(/bbox:/g, '"bbox:"'));
-                                if (parsedBody && parsedBody !== undefined && parsedBody !== "") {
-                                    if (parsedBody.hasOwnProperty('inferences')) {
-                                        if (parsedBody.inferences.length > 0) {
-                                            frames.push(parsedBody);
-                                        }
-                                    }
-                                }
-                            } catch (e) {
-                                console.log(e);
-                            }
+        // hours and minutes need to be 2 digits
+        const hours = date.getUTCHours();
+        let hoursString = `${hours}`;
+        const hoursStringLength = hoursString.length;
+        if (hoursStringLength < 2) {
+            hoursString = `0${hours}`;
+        }
+        const minutes = date.getUTCMinutes();
+        let minutesString = `${minutes}`;
+        const minutesStringLength = minutesString.length;
+        if (minutesStringLength < 2) {
+            minutesString = `0${minutes}`;
+        }
+
+        const date = dates[d];
+        const dateLocaleString = date.toLocaleDateString('fr-CA', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'
+        });
+        const dateString = dateLocaleString.replace(/-/g, '/')
+
+        const blobName = `${this.props.iotHubName}/0${this.state.blobPartition}/${dateString}/${hoursString}/${minutesString}`;
+
+        const exists = await this.blobExists(this.containerName, blobName);
+        if (exists) {
+            const containerClient = this.blobServiceClient.getContainerClient(this.containerName);
+            let blobs = containerClient.listBlobsByHierarchy("/", { prefix: blobName });
+            for await (const blob of blobs) {
+                const items = await this.getItems(this.containerName, blob.name);
+                for (const item in items) {
+                    if (item.hasOwnProperty('inferences')) {
+                        for (const inference in item.inferences) {
+                            inferences.push(inference);
                         }
                     }
                 }
             }
         }
 
-        return frames;
+        return inferences;
     }
 
-    async blobToString(blob) {
-        const fileReader = new FileReader();
-        return new Promise((resolve, reject) => {
-            fileReader.onloadend = (ev) => {
-                resolve(ev.target.result);
-            };
-            fileReader.onerror = reject;
-            fileReader.readAsText(blob);
-        });
+    async getItems(containerName, blobName) {
+        const items = [];
+
+        const containerClient = this.blobServiceClient.getContainerClient(containerName);
+        const blobClient = containerClient.getBlobClient(blobName);
+        const downloadBlockBlobResponse = await blobClient.download();
+
+        const blobString = await this.blobToString(await downloadBlockBlobResponse.blobBody);
+        const blobs = blobString.replace(/\\"/g, /'/).split('\r\n');
+
+        const l = blobs.length;
+        for (let i = 0; i < l; i++) {
+            const blob = blobs[i];
+            if (blob && blob !== undefined && blob !== "") {
+                try {
+                    let parsedBlob = JSON.parse(blob);
+                    if (parsedBlob.hasOwnProperty('Body')) {
+                        let body = parsedBlob.Body;
+                        if (body && body !== undefined && body !== "") {
+                            let decodedBody = atob(blobBody);
+                            if (decodedBody && decodedBody !== undefined && decodedBody !== "") {
+                                try {
+                                    let parsedBody = JSON.parse(decodedBody);
+                                    if (parsedBody && parsedBody !== undefined && parsedBody !== "") {
+                                        items.push(parsedBody);
+                                    }
+                                } catch (e) {
+                                    console.log(e);
+                                }
+                            }
+                        }
+                    }
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        }
+
+        return items;
     }
 }
