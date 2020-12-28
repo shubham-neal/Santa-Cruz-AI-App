89730cc10cbea8bcf5a8ffc9c9a2f578946b69f6
shubhamv <shubhamv@nealanalytics.com>
2020-12-28 11:27:59 +0000

    Updated mariner-setup.sh

--- edge-solution/people-detection/mariner/scripts/mariner-setup.sh
+++ edge-solution/people-detection/mariner/scripts/mariner-setup.sh
@@ -259,9 +259,8 @@ DEPLOYMENT_NAME=${DEPLOYMENT_NAME}${RANDOM_NUMBER}
 
 
 #required credentials
-TENANT_ID=$(az account show | jq '.tenantId')
-echo "testing"
-echo "$TENANT_ID"
+TENANT_ID=$(az account show | jq -r '.tenantId')
+
 # Getting the details of subscriptions which user has access, in case when value is not provided in variable.template
 if [ -z "$SUBSCRIPTION_ID" ]; then
     # Value is empty for SUBSCRIPTION_ID
@@ -371,8 +370,7 @@ fi
 
 MANIFEST_TEMPLATE_NAME="deployment.lvaedge.template.json"
 MANIFEST_ENVIRONMENT_VARIABLES_FILENAME=".env"
-echo "testing."
-echo "$TENANT_ID"
+
 # Update the value of RUNTIME variable in environment variable file
 sed -i 's#^\(SP_APP_ID[ ]*=\).*#\1\"'"$SP_APP_ID"'\"#g' "$MANIFEST_ENVIRONMENT_VARIABLES_FILENAME"
 sed -i 's#^\(SP_APP_PWD[ ]*=\).*#\1\"'"$SP_APP_PWD"'\"#g' "$MANIFEST_ENVIRONMENT_VARIABLES_FILENAME"
